ea4ac86c96bd381c35e94a7c73319a728bdfeffb
Gaby Nava <111267238+chimalii@users.noreply.github.com>
2022-12-01 21:00:18 -0600

    Update perro.h
    Archivo con clases que componen inscripcion.h

--- perro.h
+++ perro.h
@@ -4,6 +4,66 @@
 
 using namespace std;
 
+class Participante{
+    private:
+       string nombre, genero;
+       int edad;
+
+    public:
+       // Inicialización del constructor con datos de participante
+        
+        Participante(): nombre (""), genero (""), edad (0) {}
+        Participante(string nom, string gen, int ed): 
+          nombre (nom), genero(gen), edad(ed) {}
+
+        // Getters y setters
+        string get_nom();
+        string get_gen();
+        int get_ed();
+
+        void set_nom();
+        void set_gen();
+        void set_ed();
+
+        // Funciones adicionales
+        string toString();
+};
+
+void Participante::set_nom(){
+    cout << "Sobre el PARTICIPANTE, ingresa lo solicitado." << "\n";
+    cout << "Nombre completo: ";
+    getline(cin, nombre);
+}
+
+void Participante::set_gen(){
+    cout << "Genero: (Masculino/Femenino) ";
+    cin >> genero;
+}
+void Participante::set_ed(){
+    cout << "Edad: ";
+    cin >> edad; cout << "\n";
+}
+
+string Participante::get_nom(){
+    return nombre;
+}
+
+string Participante::get_gen(){
+    return genero;
+}
+
+int Participante::get_ed(){
+    return edad;
+}
+
+string Participante:: toString(){
+    stringstream aux;
+    aux << "========= Datos Participante =========" << "\n"
+    << "Nombre: " << nombre << "\n"
+    << "Genero: " << genero << " / " << "Edad: " << edad;
+    return aux.str();
+}
+
 class Perro{
 
     // Atributos
@@ -20,19 +80,32 @@ class Perro{
         Perro(string nom_pe, string raz, float ed_pe, bool sext, bool desp):
             nombre_perro(nom_pe), raza(raz), edad_perro(ed_pe), sextuple(sext), desparacitacion(desp) {};
 
-        // Getters
-        string get_nomperro();
+        // Getters y setters
+
+        void set_datospe();
+
+        string get_nompe();
         string get_raza();
-        float get_edperro();
+        float get_edpe();
         bool get_sext();
-        bool get_desp(); 
+        bool get_desp();
+        string toString(); 
 
         // Funciones adicionales
-        string toString();
 
 };
 
-string Perro::get_nomperro(){
+void Perro::set_datospe(){
+    cout << "Sobre el PERRO, ingresa lo solicitado." << "\n";
+    cout << "Nombre: "; cin >> nombre_perro;
+    cout << "Raza: "; cin >> raza; 
+    cout << "Edad: "; cin >> edad_perro;
+    cout << "Vacunacion (1 = si / 0 = no)" << "\n"; 
+    cout << "Sextuple --> "; cin >> sextuple;
+    cout << "Desparacitacion --> "; cin >> desparacitacion; cout << "\n";
+}
+
+string Perro::get_nompe(){
     return nombre_perro;
 }
 
@@ -40,7 +113,7 @@ string Perro::get_raza(){
     return raza;
 }
 
-float Perro::get_edperro(){
+float Perro::get_edpe(){
     return edad_perro;
 }
 
@@ -53,10 +126,73 @@ bool Perro::get_desp(){
 }
 
 string Perro::toString(){
-    stringstream aux;
-    aux << "========= Datos Perro =========" << "\n" << "Nombre: " << nombre_perro
+    stringstream aux1;
+    aux1 << "========= Datos Perro =========" << "\n" << "Nombre: " << nombre_perro
     << " / " << "Raza: " << raza << " / " << "Edad: " << edad_perro << "\n"
     << "Vacunacion: Sextuple --> " << sextuple << " / " << "Desparacitacion --> "
-    << desparacitacion << "\n";
-    return aux.str();
+    << desparacitacion;
+    return aux1.str();
+}
+
+class Carrera{
+
+  // Atributos
+    private: 
+        string modalidad, num_comp, afiliacion;     
+  
+  // Métodos  
+    public:
+        Carrera(): modalidad (""), num_comp(""), afiliacion("") {} 
+        Carrera(string mod, string comp, string afil): modalidad(mod), num_comp (comp), afiliacion(afil)  {};
+
+  // Getters y setters 
+    void set_mod();
+    void set_num();
+    void set_afil();
+
+    string get_mod();
+    string get_num();
+    string get_afil();
+    
+  // Funciones adicionales
+    string imprime_DID();
+
+};
+
+void Carrera::set_mod(){
+    cout << "Sobre MODALIDAD, ingresa lo solicitado." << "\n";
+    cout << "Modalidad: ";
+    cin >> modalidad;
+}
+
+void Carrera::set_num(){
+    cout << "Numero de competidor: ";
+    cin >> num_comp; cout << "\n";
+}
+
+void Carrera::set_afil(){
+    afiliacion = num_comp[0];
+}
+
+string Carrera::get_mod(){
+    return modalidad;
+}
+
+string Carrera::get_num(){
+    return num_comp;
+}
+
+string Carrera::get_afil(){
+    string DID;
+    if (afiliacion == "0") {DID = "No tiene DID"; afiliacion = DID;}
+    else {DID = "Tiene DID"; afiliacion = DID;}
+    return afiliacion;
+}
+
+string Carrera::imprime_DID(){
+    stringstream aux2;
+    aux2 << "========= Datos Modalidad =========" << "\n"
+    << "Modalidad: " << modalidad << "\n"
+    << "Numero de competidor: " << num_comp << " / " << afiliacion;
+    return aux2.str();
 }
